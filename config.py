626a759bff0ef3e7066eb6abd58de2e729e89285
sai piske <saipiske@gmmail.com>
2026-09-07 20:35:25 +0530

    fourth commit

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "zF>D4So.0Oq$rng>~6|<N<64#SGH")
+SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
 
 AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
 S3_BUCKET = os.getenv("S3_BUCKET", "piske-gram-bucket")
@@ -14,9 +14,9 @@ ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "gif", "webp"}
 MAX_FILE_SIZE = 10 * 1024 * 1024
 
 DB_CONFIG = {
-    "host": os.getenv("MYSQL_HOST", "10.0.1.184/24"),
+    "host": os.getenv("MYSQL_HOST", "localhost"),
     "port": int(os.getenv("MYSQL_PORT", "3306")),
     "user": os.getenv("MYSQL_USER", "admin"),
-    "password": os.getenv("MYSQL_PASSWORD", "zF>D4So.0Oq$rng>~6|<N<64#SGH"),
+    "password": os.environ["MYSQL_PASSWORD"],
     "database": os.getenv("MYSQL_DATABASE", "piskegram"),
 }
